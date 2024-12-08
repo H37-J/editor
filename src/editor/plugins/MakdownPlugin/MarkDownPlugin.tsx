7bed734c516a1362363a2a76753cad060e6aa836
hjk <these990712@gmail.com>
2024-12-08 13:16:34 +0900

    컬러, 테이블 플러그인 추가, 이그노어 추가

--- src/editor/plugins/MakdownPlugin/MarkDownPlugin.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
-// @ts-ignore
-import { TRANSFORMERS } from "@lexical/markdown";
-export const MarkdownPlugin: FC = () => {
-  return <MarkdownShortcutPlugin transformers={TRANSFORMERS} />;
-};
--- src/server/prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient}
-
-const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"]
-});
-export default prisma;
-
