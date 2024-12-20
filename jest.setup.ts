b9c70eda723f7675d2bbaa7809a9efbf6f3b2f70
hjk <these990712@gmail.com>
2024-12-21 00:53:46 +0900

    최초 배포작업 시작

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,15 +1,15 @@
-import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
-import { PrismaClient } from '.prisma/client';
-
-import prisma from './src/server/prisma'
-
-jest.mock('./src/server/prisma', () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(),
-}))
-
-beforeEach(() => {
-  mockReset(prismaMock)
-})
-
-export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
+// import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
+// import { PrismaClient } from '.prisma/client';
+//
+// import prisma from './src/server/prisma'
+//
+// jest.mock('./src/server/prisma', () => ({
+//   __esModule: true,
+//   default: mockDeep<PrismaClient>(),
+// }))
+//
+// beforeEach(() => {
+//   mockReset(prismaMock)
+// })
+//
+// export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
