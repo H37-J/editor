503a202894910eb297106bd8e08572187bdac331
hjk <these990712@gmail.com>
2024-12-21 02:01:17 +0900

    최초 배포작업 시작

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,7 +4,7 @@ import { authOptions } from '@/server/auth/auth';
 
 
 const auth = (req: NextApiRequest, res: NextApiResponse) => {
-  return NextAuth(req, res, authOptions(req, res));
+  return NextAuth(req, res, authOptions);
 };
 
 export default auth;
