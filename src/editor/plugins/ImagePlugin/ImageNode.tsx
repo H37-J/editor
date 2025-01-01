59d765897233f09c4452fffa12d9859ee868b21d
hjk <these990712@gmail.com>
2025-01-02 04:01:24 +0900

    오류 4차 수정

--- src/editor/plugins/ImagePlugin/ImageNode.tsx
+++ src/editor/plugins/ImagePlugin/ImageNode.tsx
@@ -221,7 +221,7 @@ export class ImageNode extends DecoratorNode<JSX.Element> {
 export const $createImageNode = ({
   altText,
   height,
-  maxWidth = 380,
+  maxWidth = 360,
   captionsEnabled,
   src,
   width,
